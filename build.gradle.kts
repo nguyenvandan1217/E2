3aa4e5a13b90d7248f776b14da7e45ac83f0f13b
thang-do <87466168+nguyenvandan1217@users.noreply.github.com>
2023-05-23 11:31:07 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    //trick: for the same plugin versions in all sub-modules
+    id("com.android.library").version("7.4.2").apply(false)
+    kotlin("multiplatform").version("1.8.10").apply(false)
+    kotlin("native.cocoapods") version "1.8.21"
+}
+
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
